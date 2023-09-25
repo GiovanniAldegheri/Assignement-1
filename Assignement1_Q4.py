b7fddfc99523659a31f8a6768569d28e91c28a8e
GiovanniAldegheri <s233544@dtu.dk>
2023-09-25 10:49:51 +0200

    q4 init, q3 graph

---
+++ Assignement1_Q4.py
@@ -0,0 +1,14 @@
+import math as m
+import numpy as np
+import matplotlib.pyplot as plt
+
+Cp_max = 0.469
+rou = 1.225
+R = 89.17
+area = m.pi*R**2
+P = 10.64e6
+TSR = 8
+cut_in = 4
+cut_out = 25
+Vo_rat = 11.403
+w_max = 1.023
--- Assignment1_Q3.py
+++ Assignment1_Q3.py
@@ -1,13 +1,33 @@
 import math as m
+import numpy as np
+import matplotlib.pyplot as plt
 
 Cp_max = 0.469
 rou = 1.225
 R = 89.17
-area = R*m.pi**2
+area = m.pi*R**2
 P = 10.64e6
 TSR = 8
+cut_in = 4
+cut_out = 25
 
-Vo = (P/(0.5*Cp_max*rou*area))**(1/3)
+Vo_rat = (P/(0.5*Cp_max*rou*area))**(1/3)
+w_max = TSR*Vo_rat/R
+
+print('Rated wind speed:', round(Vo_rat,3), '\nRotational speed:',round(w_max,3))
+
+Vo = np.arange(0,40,0.1)
 w = TSR*Vo/R
 
-print(round(w,3))
+for i in range(len(Vo)):
+    if Vo[i] > cut_out:
+        w[i] = 0
+    elif Vo[i] > Vo_rat:
+        w[i] = w[i-1]
+    elif Vo[i] < cut_in:
+        w[i] = 0
+
+plt.plot(Vo, w)
+plt.xlabel('Vo(m/s)')
+plt.ylabel('w(rad/s)')
+plt.show()
